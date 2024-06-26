4a7377324d2e26fe4574e082bbb8c603e5267f62
VijayKumarManthenaRelu <VijayKumarManthena@reluconsultancy.in>
2024-06-26 16:36:05 +0530

    removed unused import

--- carwash/sitewash/sitewatch_weekly.py
+++ carwash/sitewash/sitewatch_weekly.py
@@ -12,7 +12,7 @@ from sitewatch4 import sitewatchClient
 import sys
 # Add the carwash directory to the sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from tg_sender.telegram import telegramBot
+
 
 
 tg_message=[]
